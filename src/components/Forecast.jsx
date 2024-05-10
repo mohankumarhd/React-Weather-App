5e43e1b25b4cc9f6f4638dff40eeae74270c3a22
mohanhd <mohan.h.d@ibm.com>
2024-05-10 11:13:27 +0530

    Formatted list page

--- src/components/Forecast.jsx
+++ src/components/Forecast.jsx
@@ -30,74 +30,95 @@ function Forecast(props) {
     <>
       <h2>Forecast Weather</h2>
 
-      <div className="col-lg-3">
-        <div className="card m-1">
+      <div className="col-lg-10 mx-auto mb-2" style={{ height: "380px" }}>
+        <div className="card my-2 shadow">
           <div className="card-body">
-            {day1 != null && day1.weather[0] != null ? (
-              <h5>{"Description: " + day1.weather[0].description}</h5>
-            ) : null}
+            <table className="table">
+              <thead>
+                <tr>
+                  <th>Day 1</th>
+                  <th>Day 2</th>
+                  <th>Day 3</th>
+                  <th>Day 4</th>
+                  <th>Day 5</th>
+                </tr>
+              </thead>
+              <tbody>
+                <tr>
+                  <td>
+                    {" "}
+                    {day1 != null && day1.weather[0] != null ? (
+                      <h5>{"Description: " + day1.weather[0].description}</h5>
+                    ) : null}
+                    {day1 != null ? (
+                      <p>{"Temperaure: " + day1.main.temp}</p>
+                    ) : null}
+                    {day1 != null ? (
+                      <p>{"Humidity: " + day1.main.humidity}</p>
+                    ) : null}
+                    {day1 != null ? <p>{"Speed: " + day1.wind.speed}</p> : null}
+                  </td>
+                  <td>
+                    {day2 != null && day2.weather[0] != null ? (
+                      <h5>{"Description: " + day2.weather[0].description}</h5>
+                    ) : null}
 
-            {day1 != null ? <p>{"Temperaure: " + day1.main.temp}</p> : null}
+                    {day2 != null ? (
+                      <p>{"Temperaure: " + day2.main.temp}</p>
+                    ) : null}
 
-            {day1 != null ? <p>{"Humidity: " + day1.main.humidity}</p> : null}
+                    {day2 != null ? (
+                      <p>{"Humidity: " + day2.main.humidity}</p>
+                    ) : null}
 
-            {day1 != null ? <p>{"Speed: " + day1.wind.speed}</p> : null}
-          </div>
-        </div>
+                    {day2 != null ? <p>{"Speed: " + day2.wind.speed}</p> : null}
+                  </td>
+                  <td>
+                    {day3 != null && day3.weather[0] != null ? (
+                      <h5>{"Description: " + day3.weather[0].description}</h5>
+                    ) : null}
 
-        <div className="card m-1">
-          <div className="card-body">
-            {day2 != null && day2.weather[0] != null ? (
-              <h5>{"Description: " + day2.weather[0].description}</h5>
-            ) : null}
+                    {day3 != null ? (
+                      <p>{"Temperaure: " + day3.main.temp}</p>
+                    ) : null}
 
-            {day2 != null ? <p>{"Temperaure: " + day2.main.temp}</p> : null}
+                    {day3 != null ? (
+                      <p>{"Humidity: " + day3.main.humidity}</p>
+                    ) : null}
 
-            {day2 != null ? <p>{"Humidity: " + day2.main.humidity}</p> : null}
+                    {day3 != null ? <p>{"Speed: " + day3.wind.speed}</p> : null}
+                  </td>
+                  <td>
+                    {day4 != null && day4.weather[0] != null ? (
+                      <h5>{"Description: " + day4.weather[0].description}</h5>
+                    ) : null}
 
-            {day2 != null ? <p>{"Speed: " + day2.wind.speed}</p> : null}
-          </div>
-        </div>
+                    {day4 != null ? (
+                      <p>{"Temperaure: " + day4.main.temp}</p>
+                    ) : null}
 
-        <div className="card m-1">
-          <div className="card-body">
-            {day3 != null && day3.weather[0] != null ? (
-              <h5>{"Description: " + day3.weather[0].description}</h5>
-            ) : null}
+                    {day4 != null ? (
+                      <p>{"Humidity: " + day4.main.humidity}</p>
+                    ) : null}
 
-            {day3 != null ? <p>{"Temperaure: " + day3.main.temp}</p> : null}
-
-            {day3 != null ? <p>{"Humidity: " + day3.main.humidity}</p> : null}
-
-            {day3 != null ? <p>{"Speed: " + day3.wind.speed}</p> : null}
-          </div>
-        </div>
-
-        <div className="card m-1">
-          <div className="card-body">
-            {day4 != null && day4.weather[0] != null ? (
-              <h5>{"Description: " + day4.weather[0].description}</h5>
-            ) : null}
-
-            {day4 != null ? <p>{"Temperaure: " + day4.main.temp}</p> : null}
-
-            {day4 != null ? <p>{"Humidity: " + day4.main.humidity}</p> : null}
-
-            {day4 != null ? <p>{"Speed: " + day4.wind.speed}</p> : null}
-          </div>
-        </div>
-
-        <div className="card m-1">
-          <div className="card-body">
-            {day5 != null && day5.weather[0] != null ? (
-              <h5>{"Description: " + day5.weather[0].description}</h5>
-            ) : null}
-
-            {day5 != null ? <p>{"Temperaure: " + day5.main.temp}</p> : null}
-
-            {day5 != null ? <p>{"Humidity: " + day5.main.humidity}</p> : null}
-
-            {day5 != null ? <p>{"Speed: " + day5.wind.speed}</p> : null}
+                    {day4 != null ? <p>{"Speed: " + day4.wind.speed}</p> : null}
+                  </td>
+                  <td>
+                    {" "}
+                    {day5 != null && day5.weather[0] != null ? (
+                      <h5>{"Description: " + day5.weather[0].description}</h5>
+                    ) : null}
+                    {day5 != null ? (
+                      <p>{"Temperaure: " + day5.main.temp}</p>
+                    ) : null}
+                    {day5 != null ? (
+                      <p>{"Humidity: " + day5.main.humidity}</p>
+                    ) : null}
+                    {day5 != null ? <p>{"Speed: " + day5.wind.speed}</p> : null}
+                  </td>
+                </tr>
+              </tbody>
+            </table>
           </div>
         </div>
       </div>
